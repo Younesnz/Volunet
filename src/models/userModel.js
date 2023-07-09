d82cb6cc618e603956483ee4f846fd0d3464616f
Ata <ataserb@gmail.com>
2023-07-09 18:04:13 +0300

    #6: add controller methods, routes, TODO: add auth parameters to routers after auth middlewares are coded

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,6 +16,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 UserSchema.pre('save', async function (next) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,13 @@ const userController = require('/userController');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
+// TODO: these should have authentication for users
+router.get('/profile/:id', userController.getUserProfile);
+router.put('/profile/:id', userController.updateUserProfile);
+// TODO: these should have admin authorization
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
 module.exports = router;
