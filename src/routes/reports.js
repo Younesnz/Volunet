3411ceadb536f1ce99f77d91c8b782279da87e0e
Younesnz <107856670+Younesnz@users.noreply.github.com>
2023-07-12 08:07:52 +0300

    #10: Report model and controller

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-// const UserController = require('../controllers/reportController');
+const reportController = require('../controllers/reportController');
 
-// This is just for testing
-router.get('/', (req, res) => res.json({ success: true }));
+// getReports can be filtered by query
+// acceptable queries: status, category, after(time), before(time), userId, adminId, eventId
+router.get('/', reportController.getReports);
+router.post('/', reportController.addReport);
+router.get('/:id', reportController.getReportById);
+router.put('/:id', reportController.updateReportById);
+router.delete('/:id', reportController.deleteReportById);
 
 module.exports = router;
