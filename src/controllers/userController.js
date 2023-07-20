35b82c7d5e07a61596518f76fd3e3e629c0b30ad
Younes Nourzehi <unesnz@gmail.com>
2023-07-20 18:39:33 +0300

    update user model and controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,157 +1,251 @@
+const debug = require('debug')('app:userController');
 const jwt = require('jsonwebtoken');
-
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
+const passwordComplexity = require('joi-password-complexity');
 
 const User = require('../models/userModel');
+const { Application } = require('../models/applicationModel');
+const { Event } = require('../models/eventModel');
+
+const {
+  success,
+  validationError,
+  errorResponse,
+} = require('../utils/responseUtils');
 
 // Define validation
+
 const registerSchema = Joi.object({
-    username: Joi.string().min(4).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+  username: Joi.string().min(4).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(50).required(),
+  first_name: Joi.string().max(20),
+  last_name: Joi.string().max(20),
+  birthDate: Joi.date().iso(),
+  profilePic: Joi.string(),
+  country: Joi.string().max(20).required(),
+  city: Joi.string().max(20).required(),
 });
 
+const updateUserSchema = Joi.object({
+  username: Joi.string().min(4).max(30),
+  email: Joi.string().email(),
+  password: Joi.string().min(8).max(50),
+  first_name: Joi.string().max(20),
+  last_name: Joi.string().max(20),
+  birthDate: Joi.date().iso(),
+  profilePic: Joi.string(),
+  country: Joi.string().max(20),
+  city: Joi.string().max(20),
+  lon: Joi.number().min(-180).max(180),
+  lat: Joi.number().min(-90).max(90),
+});
 const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 exports.registerUser = async (req, res) => {
-    const { error } = registerSchema.validate(req.body);
+  const { error } = registerSchema.validate(req.body);
 
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
-    }
+  if (error) {
+    return res
+      .status(400)
+      .json(validationError(error, error.details[0].message));
+  }
 
-    const { username, email, password } = req.body;
+  const { error: passwordError } = passwordComplexity({
+    min: 8,
+    max: 50,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 0,
+    requirementCount: 3,
+  }).validate(req.body.password);
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+  if (passwordError)
+    return res
+      .status(400)
+      .json(
+        validationError(
+          passwordError,
+          'Password should be at least 8 characters long, including both uppercase and lowercase letters, and at least one number.'
+        )
+      );
 
-        const user = new User({
-            username,
-            email,
-            password: hashedPassword,
-        });
+  const { username, email, password } = req.body;
 
-        await user.save();
-        return res
-            .status(201)
-            .json({ message: 'User registered successfully' });
-    } catch (err) {
-        return res.status(500).json({ error: err.message });
-    }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    const result = await user.save();
+    return res
+      .status(201)
+      .json(success(result, 'User registered successfully'));
+  } catch (err) {
+    debug(`Error in registerUser: ${err}`);
+    return res
+      .status(500)
+      .json(
+        errorResponse('Internal Server Error! failed to register the User.')
+      );
+  }
 };
 
 exports.loginUser = async (req, res) => {
-    const { error } = loginSchema.validate(req.body);
+  const { error } = loginSchema.validate(req.body);
 
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
+  if (error) {
+    return res
+      .status(400)
+      .json(validationError(error, error.details[0].message));
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return res
+        .status(401)
+        .json(errorResponse('Invalid email or password', 401));
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json(errorResponse('Invalid email or password', 401));
     }
 
-    const { email, password } = req.body;
+    const { _id: id, username } = user;
+    const token = jwt.sign({ id }, process.env.JWT_SECRET);
 
-    try {
-        const user = await User.findOne({ email }).select('+password');
-
-        if (!user) {
-            return res
-                .status(400)
-                .json({ message: 'Invalid email or password' });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res
-                .status(400)
-                .json({ message: 'Invalid email or password' });
-        }
-
-        const { _id: id, username } = user;
-        const token = jwt.sign({ id }, process.env.JWT_SECRET);
-
-        return res.json({
-            token,
-            user: {
-                id,
-                username,
-                email,
-            },
-        });
-    } catch (err) {
-        return res.status(500).json({ error: err.message });
-    }
+    return res.json(
+      success(
+        {
+          token,
+          user: {
+            id,
+            username,
+            email,
+          },
+        },
+        'user logged in successfully.'
+      )
+    );
+  } catch (err) {
+    debug(`Error in loginUser: ${err}`);
+    return res
+      .status(500)
+      .json(errorResponse('Internal Server Error! failed to login the User.'));
+  }
 };
 
 // Authenticated User Controllers
 
 exports.getUserProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id).select('-password');
-        if (!user) throw Error('User does not exist');
-        return res.json(user);
-    } catch (e) {
-        return res.status(400).json({ msg: e.message });
-    }
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id).select('-password -__v');
+    if (!user) res.status(404).json(errorResponse('User not found!'));
+    return res
+      .status(200)
+      .json(success(user, 'User profile fetched successfully'));
+  } catch (err) {
+    if (err instanceof mongoose.CastError)
+      return res.status(400).json(errorResponse('Invalid User ID!', 400));
+
+    debug(`Error in getUserProfile: ${err}`);
+    return res
+      .status(500)
+      .json(errorResponse('Internal Server Error! failed to fetch user data.'));
+  }
 };
 
 exports.updateUserProfile = async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!user) throw Error('User does not exist');
+  try {
+    const { error } = updateUserSchema.validate(req.body);
+    if (error)
+      return res
+        .status(400)
+        .json(validationError(error, error.details[0].message));
 
-        return res.json(user);
-    } catch (e) {
-        return res.status(400).json({ msg: e.message });
-    }
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!user)
+      return res
+        .status(404)
+        .json(errorResponse(`User with ID ${id} not found!`));
+
+    return res
+      .status(200)
+      .json(success(user, 'User profile updated successfully'));
+  } catch (err) {
+    if (err instanceof mongoose.CastError)
+      return res.status(400).json(errorResponse('Invalid User ID!', 400));
+
+    debug(`Error in updateUserProfile: ${err}`);
+    return res
+      .status(500)
+      .json(
+        errorResponse('Internal Server Error! failed to update user data.')
+      );
+  }
 };
 
 // Admin Controllers
 
 exports.getUsers = async (req, res) => {
-    try {
-        const users = await User.find(req.query);
+  try {
+    const users = await User.find(req.query);
 
-        return res.json(users);
-    } catch (e) {
-        return res.status(400).json({ msg: e.message });
-    }
+    return res.json(users);
+  } catch (e) {
+    return res.status(400).json({ msg: e.message });
+  }
 };
 
 exports.getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) throw Error('User does not exist');
-        res.json(user);
-    } catch (e) {
-        res.status(400).json({ msg: e.message });
-    }
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) throw Error('User does not exist');
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 };
 
 exports.updateUserById = async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!user) throw Error('User does not exist');
-        res.json(user);
-    } catch (e) {
-        res.status(400).json({ msg: e.message });
-    }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) throw Error('User does not exist');
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 };
 
 exports.deleteUserById = async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) throw Error('User does not exist');
-        res.json({ msg: 'User deleted successfully' });
-    } catch (e) {
-        res.status(400).json({ msg: e.message });
-    }
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) throw Error('User does not exist');
+    res.json({ msg: 'User deleted successfully' });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,90 +3,127 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const NotificationSchema = new mongoose.Schema({
-    title: String,
-    message: String,
-    status: {
-        type: String,
-        enum: ['unread', 'read'],
-        default: 'unread',
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now,
-    },
+  title: String,
+  message: String,
+  status: {
+    type: String,
+    enum: ['unread', 'read'],
+    default: 'unread',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const locationSchema = new mongoose.Schema({
-    point: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true,
-        },
-        coordinates: {
-            type: [Number], // two items, first longitude, and second latitude
-            required: true,
-        },
+  point: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
     },
-    country: {
-        type: String,
-        required: true,
+    coordinates: {
+      type: [Number], // two items, first longitude, and second latitude
+      required: true,
     },
-    city: {
-        type: String,
-        required: true,
-    },
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+const RatedEventSchema = new mongoose.Schema({
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
 });
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Please provide a username'],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, 'Please add a password'],
-        minlength: 6,
-        select: false,
-    },
-    email: {
-        type: String,
-        required: [true, 'Please provide an email'],
-        unique: true,
-        match: [
-            /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/,
-            'Please provide a valid email',
-        ],
-    },
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 8,
+    maxlength: 50,
+    select: false,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    match: [
+      /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      'Please provide a valid email',
+    ],
+  },
 
-    googleID: String,
-    first_name: String,
-    last_name: String,
-    birthDate: Date,
-    isVerified: {
-        type: Boolean,
-        default: false,
+  googleID: String,
+  first_name: String,
+  last_name: String,
+  birthDate: Date,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  profilePic: String,
+  location: locationSchema,
+  notifications: [NotificationSchema],
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'organization'],
+    default: 'user',
+  },
+
+  ratedEvents: [RatedEventSchema],
+  likedEvents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
     },
-    profile_picture: String,
-    location: locationSchema,
-    notifications: [NotificationSchema],
-    role: {
-        type: String,
-        enum: ['user', 'admin', 'organization'],
-        default: 'user',
+  ],
+  joinedEvents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
     },
+  ],
+  createdEvents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
+    },
+  ],
 });
 
 UserSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 UserSchema.methods.comparePassword = function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
