2db83e2bc47729b4ff8bfc82e6192f9444cd2f22
Abdulrahman <albakkar997@gmail.com>
2023-08-05 13:18:46 +0300

    #32: Swagger documentation added.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ require('dotenv').config('../.env');
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 require('./middleware/googleAuth');
 
@@ -10,6 +12,26 @@ const debug = require('debug')('app:server');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const swaggerDefinitions = require('./swaggerDefinitions');
+// Swagger configuration
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Volunet API',
+      version: '1.0.0',
+      description: 'API Documentation for Volunet',
+    },
+    components: {
+      schemas: swaggerDefinitions,
+    },
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(
   session({
@@ -26,13 +48,11 @@ app.use(passport.session());
 // Initialize DB
 require('./db')();
 
-// const usersRoute = require('./routes/users');
 const applicationsRoute = require('./routes/applications');
 const eventsRoute = require('./routes/events');
 const reportsRoute = require('./routes/reports');
 const usersRoute = require('./routes/users');
 
-// app.use('/api/v1/users/', usersRoute);
 app.use('/api/v1/applications/', applicationsRoute);
 app.use('/api/v1/events/', eventsRoute);
 app.use('/api/v1/reports/', reportsRoute);
