bccc22a00eedb672a38ec626d3ee48603972a2ae
Ata <ataserb@gmail.com>
2023-08-10 01:50:36 +0300

    #tests: add model and middleware tests, remove every env variable in tests

--- src/__tests__/middleware/auth.test.js
+++ src/__tests__/middleware/auth.test.js
@@ -1,56 +1,76 @@
-const httpMocks = require('node-mocks-http');
 const jwt = require('jsonwebtoken');
 const { authenticate, adminOnly } = require('../../middleware/auth');
 const User = require('../../models/userModel');
-require('dotenv').config();
+
+jest.mock('jsonwebtoken');
+jest.mock('../../models/userModel');
 
 describe('Authentication Middleware', () => {
   describe('authenticate', () => {
     it('should call next if authentication is successful', async () => {
-      const user = new User({ _id: 'someUserId', role: 'user' });
-      jest.spyOn(User, 'findOne').mockResolvedValueOnce(user);
-      const token = jwt.sign(
-        { id: 'someUserId', iat: Math.floor(Date.now() / 1000) },
-        process.env.JWT_SECRET
-      );
-
-      const req = httpMocks.createRequest({
-        headers: { Authorization: token },
-      });
-      const res = httpMocks.createResponse();
+      const req = {
+        header: jest.fn().mockReturnValue('Bearer token'),
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
       const next = jest.fn();
 
+      jwt.verify.mockReturnValue({
+        id: 'someUserId',
+        iat: Math.floor(Date.now() / 1000),
+      });
+      User.findOne.mockResolvedValue({ _id: 'someUserId', role: 'user' });
+
       await authenticate(req, res, next);
+
       expect(next).toHaveBeenCalled();
     });
 
-    it('should return unauthorized if token is not provided', async () => {
-      const req = httpMocks.createRequest();
-      const res = httpMocks.createResponse();
+    it('should return an error if token is missing', async () => {
+      const req = {
+        header: jest.fn().mockReturnValue(null),
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
       const next = jest.fn();
 
       await authenticate(req, res, next);
-      expect(res.statusCode).toBe(401);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalled();
     });
   });
 
   describe('adminOnly', () => {
     it('should call next if user is admin', async () => {
-      const req = httpMocks.createRequest({ isAdmin: true });
-      const res = httpMocks.createResponse();
+      const req = { isAdmin: true };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
       const next = jest.fn();
 
       await adminOnly(req, res, next);
+
       expect(next).toHaveBeenCalled();
     });
 
-    it('should return forbidden if user is not admin', async () => {
-      const req = httpMocks.createRequest({ isAdmin: false });
-      const res = httpMocks.createResponse();
+    it('should return an error if user is not admin', async () => {
+      const req = { isAdmin: false };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
       const next = jest.fn();
 
       await adminOnly(req, res, next);
-      expect(res.statusCode).toBe(403);
+
+      expect(res.status).toHaveBeenCalledWith(403);
+      expect(res.json).toHaveBeenCalled();
     });
   });
 });
